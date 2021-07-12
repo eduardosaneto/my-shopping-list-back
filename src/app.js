213f6fb2b552ed819a0d0d8386be36cab22f49fa
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-07-12 15:30:48 -0300

    rota get

--- src/app.js
+++ src/app.js
@@ -7,4 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/items", async (req, res) => {
+    try {
+    const list = await connection.query(`
+        SELECT * FROM myList`);
+
+    res.send(list.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
+const port = 4000;
+
 app.listen(4000, () => {
   console.log(`Server is listening on port ${port}.`);
 });
